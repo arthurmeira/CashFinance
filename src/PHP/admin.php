14f9e86f5b9e655e970a9519976968e516b25cfd
arthurmeira <arthurmeiraprog@gmail.com>
2024-01-02 20:23:01 -0300

    First commit

--- src/PHP/admin.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    include_once("session.php");
-    echo phpinfo();
-?>
-
-
-ADMIN
--- src/PHP/var.php
+++
@@ -1,6 +0,0 @@
-<?php
-error_reporting(0);
-    $login = $_POST['login_cadastro'];
-    $email = $_POST['email_cadastro'];
-    $senha = $_POST['senha_cadastro'];   
-    $nome = $_POST['nome_cadastro'];
